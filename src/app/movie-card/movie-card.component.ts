c8767031b5376bdb721a31feae124816c1c28596
Alexey Sapr <alexeysapr@gmail.com>
2022-04-16 16:07:47 +0300

    add movie-card component

---
+++ src/app/movie-card/movie-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'movie';
+
+@Component({
+  selector: 'app-movie-card',
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss'],
+})
+export class MovieCardComponent implements OnInit {
+  @Input('movieItem') movie?: Movie;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
