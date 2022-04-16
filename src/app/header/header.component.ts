6f2fe489f134a85db7c00e718f19af4ab96d1e37
Alexey Sapr <alexeysapr@gmail.com>
2022-04-16 13:11:16 +0300

    Add theme switcher

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  themeMode: string | null = localStorage.getItem('themeMode') || 'Light';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  setTheme(): void {
+    if (this.themeMode === 'Light') {
+      localStorage.setItem('themeMode', 'Dark');
+      this.themeMode = 'Dark';
+    } else {
+      localStorage.setItem('themeMode', 'Light');
+      this.themeMode = 'Light';
+    }
   }
-
 }
