518f2e40a31315d2a982144736115ea86c25090f
Alexey Sapr <alexeysapr@gmail.com>
2022-04-19 17:29:30 +0300

    update form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,7 +25,7 @@ import { FormPageComponent } from './components/form-page/form-page.component';
     MoviesPageComponent,
     FormPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/form-page/form-page.component.ts
+++ src/app/components/form-page/form-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-page',
@@ -6,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-page.component.scss'],
 })
 export class FormPageComponent implements OnInit {
-  constructor() {}
+  public form: FormGroup;
+  constructor() {
+    this.form = new FormGroup({
+      title: new FormControl(''),
+      fileImage: new FormControl(''),
+      date: new FormControl(''),
+      boxOffice: new FormControl(''),
+    });
+  }
 
   ngOnInit(): void {}
+
+  onSubmit() {
+    console.log('form: ', this.form.value);
+    this.form.reset();
+  }
 }
